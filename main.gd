d93aef7abb85b1d0f5f723a57a3050e699c731bf
Xavier Sellier <xsellier@gmail.com>
2017-12-01 19:57:50 -0500

    Hardware cursor test

--- main.gd
+++ main.gd
@@ -1,4 +1,8 @@
 extends Control
 
+const cursor_default = preload('res://cursor.png')
+const cursor_ibeam = preload('res://cursor-select.png')
+
 func _ready():
-	Input.set_mouse_mode(0)
+	Input.set_custom_mouse_cursor(cursor_default, CURSOR_ARROW, Vector2(7, 3))
+	Input.set_custom_mouse_cursor(cursor_ibeam, CURSOR_IBEAM)
